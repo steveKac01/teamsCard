33245233627fbf3259392f799b1d5ef0fd25ab4e
Steveport <aol@aol.com>
2022-02-04 09:38:56 +0100

    json modif

--- js/teamsApp_full.js
+++ js/teamsApp_full.js
@@ -127,4 +127,52 @@ function LoadCard(numberMember)
 LoadCard(iCard)
 
 //Timer
-setInterval(NextCard,2000)
+ //setInterval(NextCard,2000)
+
+ console.log("test")
+ 
+const URLJS = "https://stevekac01.github.io/teamsCard/teams.json"
+
+let a =  getData()
+
+async function getData(){
+
+    try {
+	    const response = await axios.get(URLJS);
+        const test =  response.data 
+	     console.log( test[0]);
+	 
+	  } catch (error) {
+	    console.error(error);
+	  }
+
+}
+
+/*
+ 
+ const WEATHER_API = 'https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=585ee65ca36290ecc301eab6daa59875&units=metric'
+	async function getWeather() {
+	  try {
+	    const response = await axios.get(WEATHER_API);
+	    // console.log(response);
+		updateMeteoCard(response.data)
+	  } catch (error) {
+	    console.error(error);
+	  }
+	}
+	function updateMeteoCard(result){
+		console.log(result)
+		let iconCode = result.weather[0].icon;
+		let temp     = result.main.temp;
+		document.getElementById('wicon').src=`http://openweathermap.org/img/w/${iconCode}.png`
+		document.querySelector('.temp').textContent=Math.round(temp)+'°c'
+		document.querySelector('.city-title').textContent=result.name;
+
+
+	}
+
+	let goBtn = document.querySelector('#goBtn');
+	goBtn.addEventListener('click',async function(){
+		let a = await getWeather();
+	})
+ */ 
